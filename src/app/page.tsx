803c10053438d239b884f93921aef68dc78f7094
baiyan412048 <39880236+baiyan412048@users.noreply.github.com>
2024-05-26 16:21:09 +0800

    feat: full api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,15 +24,25 @@ export default function Home() {
   const { translate, addNewTranslate } = useTranslateStore()
   const { tableInfo, addToTableInfo } = useTableInfoStore()
 
+  const [loading, setLoading] = useState(true)
+
+  const [main, setMain] = useState<{
+    id: number,
+    name: string,
+    normal: string,
+    abnormal: string,
+    option: string[]
+  }[]>([])
+
   const [allState, setAllState] = useState<{
-    [key: string]: {
+    [key: number]: {
       title: string
       state: boolean
       stateName: string
       note?: string[]
     }
   }>({})
-  const addKeyToAllState = (key: string, value: {
+  const addKeyToAllState = (key: number, value: {
     title: string
     state: boolean
     stateName: string
@@ -51,75 +61,69 @@ export default function Home() {
     })
   }
 
-  const [main, setMain] = useState<{
-    '編號': string
-    '問題類型': '主要問題',
-    '選項類型': '單選',
-    '問題名稱': string,
-    '正常選項名稱': string,
-    '異常選項名稱': string,
-    '異常問題細項': string[]
-  }[]>([])
-  const [sub, setSub] = useState<{
-    '編號': string
-    '問題類型': '從屬問題'
-    '選項類型': '多選' | '多選含輸入備註'
-    '問題名稱': string
-  }[]>([])
-
-  const { data, loading, error } = useGoogleSheets({
-    apiKey: process.env.GOOGLE_API_KEY ?? 'AIzaSyCWxiZMVqH1h94h73QL4evSRFvKFnv15mU',
-    sheetId: process.env.GOOGLE_SHEET_ID ?? '1EOQ3luyQvq38F4bJYWpUiDeUr8JPbmUxlKwosPsWOeE',
-  });
-
-  useEffect(() => {
-    if (data.length) {
-      const sheetData = data[0] as unknown as {
-        data: {
-          '編號': string
-          '問題類型': '主要問題' | '從屬問題'
-          '選項類型': '單選' | '多選' | '多選含輸入備註'
-          '問題名稱': string
-          '正常選項名稱'?: string
-          '異常選項名稱'?: string
-          '異常問題細項'?: string
-        }[]
+  const getQuestion = async () => {
+    try {
+      const res = await fetch('http://localhost:8080/api/question/date')
+      const { data } = await res.json()
+      if (!data.length) {
+        throw new Error('無法獲取題目資料')
       }
-      console.log(sheetData, 'sheetData')
-      const tempMain = sheetData.data.filter((item) => item['問題類型'] === '主要問題') as {
-        '編號': string
-        '問題類型': '主要問題'
-        '選項類型': '單選'
-        '問題名稱': string
-        '正常選項名稱'?: string
-        '異常選項名稱'?: string
-        '異常問題細項'?: string
-      }[]
-      const main = tempMain.map((item) => ({
+
+      console.log(data)
+
+      const main = (data as {
+        id: number,
+        name: string,
+        normal: string,
+        abnormal: string,
+        option: string
+      }[]).map((item) => ({
         ...item,
-        '問題名稱': item['問題名稱'].replace(/[\r\n]+/g, ''),
-        '正常選項名稱': item['正常選項名稱'] ?? '正常',
-        '異常選項名稱': item['異常選項名稱'] ?? '異常',
-        '異常問題細項': item['異常問題細項']?.split(',') ?? []
+        option: item.option.split(',') ?? []
       }))
       setMain(main)
-
-      const sub = sheetData.data.filter((item) => item['問題類型'] === '從屬問題') as {
-        '編號': string
-        '問題類型': '從屬問題'
-        '選項類型': '多選' | '多選含輸入備註'
-        '問題名稱': string
-      }[]
-      setSub(sub)
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setLoading(false)
     }
-  }, [data]);
+  }
+
+  const postReport = async () => {
+    try {
+      await fetch(`http://localhost:8080/api/report/${tableInfo.branch}/${tableInfo.box}/daily`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          info: [tableInfo],
+          report: Object.values(allState).map((item) => ({
+            ...item,
+            note: item.note?.join()
+          }))
+        })
+      })
+      alert('已成功送出表單')
+      addNewTranslate('0')
+      setAllState({})
+      addToTableInfo({
+        branch: '',
+        box: '',
+        person: ''
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    getQuestion()
   }, []);
 
   return (
-    loading ? <div>LOADING</div> : error ? <div>{JSON.stringify(error)}</div> : <main className="min-h-dvh flex flex-col" style={ {'transform': `translate3d(0, ${translate}, 0)`} }>
+    loading ? <div>LOADING</div> : <main className="min-h-dvh flex flex-col" style={ {'transform': `translate3d(0, ${translate}, 0)`} }>
       <section className="flex flex-col items-center justify-center h-dvh p-6">
         <div className="mb-20 text-center font-bold">
           <p className="mb-4 text-5xl">故鄉 KTV 巡檢表</p>
@@ -128,29 +132,29 @@ export default function Home() {
         <div className="mx-auto w-full max-w-[400px] space-y-6">
           <div>
             <div className="mb-2 text-2xl font-bold">選擇分店</div>
-            <Select value={tableInfo['分店']} onValueChange={(value) => addKeyToTableInfo('分店', value)}>
+            <Select value={tableInfo.branch} onValueChange={(value) => addKeyToTableInfo('branch', value)}>
               <SelectTrigger>
                 <SelectValue placeholder="選擇分店" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectItem value="旱溪店">旱溪店</SelectItem>
-                  <SelectItem value="大里店">大里店</SelectItem>
+                  <SelectItem value="Hanxi">旱溪店</SelectItem>
+                  <SelectItem value="Dali">大里店</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
           </div>
           <div>
             <div className="mb-2 text-2xl font-bold">包廂號碼</div>
-            <Input value={tableInfo['包廂號碼']} onBlur={() => window.scrollTo(0, 0)} onInput={(event) => addKeyToTableInfo('包廂號碼', (event.target as HTMLInputElement).value)} type="text" inputMode="numeric" placeholder="包廂號碼" />
+            <Input value={tableInfo.box} onBlur={() => window.scrollTo(0, 0)} onInput={(event) => addKeyToTableInfo('box', (event.target as HTMLInputElement).value)} type="text" inputMode="numeric" placeholder="包廂號碼" />
           </div>
           <div>
             <div className="mb-2 text-2xl font-bold">巡包人</div>
-            <Input value={tableInfo['巡包人']} onBlur={() => window.scrollTo(0, 0)} onInput={(event) => addKeyToTableInfo('巡包人', (event.target as HTMLInputElement).value)} type="text" placeholder="巡包人" />
+            <Input value={tableInfo.person} onBlur={() => window.scrollTo(0, 0)} onInput={(event) => addKeyToTableInfo('person', (event.target as HTMLInputElement).value)} type="text" placeholder="巡包人" />
           </div>
           <div className="text-center">
             <Button className="font-bold" onClick={() => {
-              if (!tableInfo['分店'] || !tableInfo['包廂號碼'] || !tableInfo['巡包人']) {
+              if (!tableInfo.branch || !tableInfo.box || !tableInfo.person) {
                 alert('請完整填寫巡檢表資訊')
                 return
               }
@@ -161,55 +165,55 @@ export default function Home() {
       </section>
       {
         main.map((item, index) => (
-          <section key={item['編號']} className="flex flex-col items-center justify-center p-6 h-dvh">
+          <section key={item.id} className="flex flex-col items-center justify-center p-6 h-dvh">
             <div className="mb-10 text-center font-bold">
-              <p className="text-3xl">{item['問題名稱']}</p>
+              <p className="text-3xl">{item.name}</p>
             </div>
             <div className="mx-auto mb-20 w-full max-w-[400px] space-y-6">
               <div>
-                <RadioGroup className="flex justify-center space-x-6" value={allState[item['編號']] ? allState[item['編號']].state ? 'normal' : 'abnormal' : undefined}>
+                <RadioGroup className="flex justify-center space-x-6" value={allState[item.id] ? allState[item.id].state ? 'normal' : 'abnormal' : undefined}>
                   <Label className='flex space-x-2'>
-                    <RadioGroupItem value="normal" onClick={() => addKeyToAllState(item['編號'], {
-                      title: item['問題名稱'],
+                    <RadioGroupItem value="normal" onClick={() => addKeyToAllState(item.id, {
+                      title: item.name,
                       state: true,
-                      stateName: item['正常選項名稱'],
+                      stateName: item.normal,
                       note: []
                     })} />
-                    <span>{item['正常選項名稱']}</span>
+                    <span>{item.normal}</span>
                   </Label>
                   <Label className='flex space-x-2'>
-                    <RadioGroupItem value="abnormal" onClick={() => addKeyToAllState(item['編號'], {
-                      title: item['問題名稱'],
+                    <RadioGroupItem value="abnormal" onClick={() => addKeyToAllState(item.id, {
+                      title: item.name,
                       state: false,
-                      stateName: item['異常選項名稱'],
-                      note: allState[item['編號']]?.note
+                      stateName: item.abnormal,
+                      note: allState[item.id]?.note
                     })} />
-                    <span>{item['異常選項名稱']}</span>
+                    <span>{item.abnormal}</span>
                   </Label>
                 </RadioGroup>
                 {
-                  (item['異常問題細項'].length && (allState[item['編號']]?.state !== undefined ? !allState[item['編號']].state : false)) && <div className="mt-6 space-y-4">
+                  (item.option.length && (allState[item.id]?.state !== undefined ? !allState[item.id].state : false)) && <div className="mt-6 space-y-4">
                     <p className='text-xl'>異常問題 :</p>
                     {
-                      sub.filter((tempSub) => item['異常問題細項'].includes(tempSub['編號'])).map((tempSub, index) => (
-                        <div key={`選項${index}${tempSub['問題名稱']}`} className="space-y-4">
-                          { tempSub['選項類型'] === '多選含輸入備註' ? <Textarea placeholder="如異常狀況為其他問題，請輸入問題描述。" /> : <Label className='flex space-x-2'>
-                              <Checkbox checked={allState[item['編號']]?.note?.includes(tempSub['問題名稱'])}  onCheckedChange={(value) => {
-                                const note = allState[item['編號']]?.note?.slice() ?? []
+                      item.option.map((option, index) => (
+                        <div key={`選項${index}${option}`} className="space-y-4">
+                          { option === '其他' ? <Textarea placeholder="如異常狀況為其他問題，請輸入問題描述。" /> : <Label className='flex space-x-2'>
+                              <Checkbox checked={allState[item.id]?.note?.includes(option)}  onCheckedChange={(value) => {
+                                const note = allState[item.id]?.note?.slice() ?? []
                                 if (value) {
-                                  note?.push(tempSub['問題名稱'])
+                                  note?.push(option)
                                 } else {
-                                  const index = note?.findIndex((target) => target === tempSub['問題名稱'])
+                                  const index = note?.findIndex((target) => target === option)
                                   index !== undefined && note?.splice(index, 1)
                                 }
-                                addKeyToAllState(item['編號'], {
-                                  title: item['問題名稱'],
+                                addKeyToAllState(item.id, {
+                                  title: item.name,
                                   state: false,
-                                  stateName: item['異常選項名稱'],
+                                  stateName: item.abnormal,
                                   note
                                 })
                               }}/>
-                              <span>{tempSub['問題名稱']}</span>
+                              <span>{option}</span>
                             </Label>
                           }
                         </div>
@@ -224,7 +228,7 @@ export default function Home() {
                 addNewTranslate(`${-100 * index}dvh`)
               }}>上一步</Button>
               <Button className="font-bold" onClick={() => {
-                if (allState[item['編號']] === undefined) {
+                if (allState[item.id] === undefined) {
                   alert('請確認所有檢核項目')
                   return
                 }
@@ -260,11 +264,7 @@ export default function Home() {
             <Button className="font-bold" variant="outline" onClick={() => {
               addNewTranslate(`${-100 * main.length}dvh`)
             }}>上一步</Button>
-            <Button className="font-bold" onClick={() => {
-              addNewTranslate('0')
-              setAllState({})
-              addToTableInfo({})
-            }}>送出表單</Button>
+            <Button className="font-bold" onClick={() => postReport()}>送出表單</Button>
           </div>
         </div>
       </section>
